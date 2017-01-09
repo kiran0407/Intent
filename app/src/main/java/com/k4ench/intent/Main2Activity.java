3834a34d6e9a549a9e2769fddc16b9313ec1d714
kiran0407 <k.kirankumar0407@gmail.com>
2017-01-09 12:57:36 +0530

    Initial commit

---
+++ app/src/main/java/com/k4ench/intent/Main2Activity.java
@@ -0,0 +1,55 @@
+package com.k4ench.intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+TextView e2;
+    Button fab;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        e2=(TextView) findViewById(R.id.textView3);
+        final Intent intent = getIntent();
+        String name = intent.getStringExtra("acd");
+        e2.setText("hi  "+name+"!!!");
+
+        setTitle(name);
+
+         fab = (Button) findViewById(R.id.button2);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);}
+
+
+
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
